e1609053b66e00feefe48fd0328961db8e1ad647
insang <dhkq92@icloud.com>
2018-08-17 18:54:26 +0900

    HomeActivity 추가, MainActivity > 2초후 HomeActivity이동하도록 수정

--- app/src/main/java/com/anywhere/anywhere/MainActivity.java
+++ app/src/main/java/com/anywhere/anywhere/MainActivity.java
@@ -1,13 +1,29 @@
 package com.anywhere.anywhere;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anywhere.anywhere.insang.activities.HomeActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    //Handler 이용 2초후 HomeActivity로 화면 전환
+    Handler handler= new Handler();
+    handler.postDelayed(new Runnable(){
+      @Override
+      public void run() {
+        Intent homeIntent= new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(homeIntent);
+        finish();
+      }
+    }, 2000);
+
+  }
 }
---
+++ app/src/main/java/com/anywhere/anywhere/insang/activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.anywhere.anywhere.insang.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anywhere.anywhere.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.home_insang);
+  }
+}
