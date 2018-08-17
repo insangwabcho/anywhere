c899a34a2772f8c0c048f95dc7c8c1f5d30460d9
min <ken1336@naver.com>
2018-08-17 20:13:31 +0900

    Add fragment to MainActivity

--- app/src/main/java/com/anywhere/anywhere/MainActivity.java
+++ app/src/main/java/com/anywhere/anywhere/MainActivity.java
@@ -1,13 +1,67 @@
 package com.anywhere.anywhere;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+import com.anywhere.anywhere.min.PageAdapter;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.icon1));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.icon2));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.icon3));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.icon4));
+        tabLayout.setSelectedTabIndicatorHeight(0);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                tabLayout.getTabAt(tab.getPosition()).getIcon().setAlpha(50);
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tabLayout.getTabAt(tab.getPosition()).getIcon().setAlpha(255);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        setViewPager();
+    }
+
+    public void setViewPager(){
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new PageAdapter(getSupportFragmentManager(),this));
+        viewPager.setCurrentItem(0);
+        viewPager.getAdapter().notifyDataSetChanged();
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+    }
+    @Override
+    public void onClick(View view) {
+
     }
 }
