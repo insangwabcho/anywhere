410812a937256c0fcf66176bca563ddf1f852e2d
insang <dhkq92@icloud.com>
2018-08-18 04:50:16 +0900

    Add Trip DTO

--- app/src/main/java/com/anywhere/anywhere/insang/models/dao/manager/TripManager.java
+++ app/src/main/java/com/anywhere/anywhere/insang/models/dao/manager/TripManager.java
@@ -2,23 +2,29 @@ package com.anywhere.anywhere.insang.models.dao.manager;
 
 import com.anywhere.anywhere.insang.models.dto.api.DayScheduleDTO;
 import com.anywhere.anywhere.insang.models.dto.api.ObjectDTO;
+import com.anywhere.anywhere.insang.models.dto.api.TripDTO;
 
 import java.util.List;
 
 public class TripManager {
 
-  private List<ObjectDTO> daySchedule;
+  private List<ObjectDTO> trip;
+  private TripDTO dto;
+
+  public TripManager(List<ObjectDTO> trip) {
+    this.trip = trip;
+  }
 
   public void insertDaySchedule(ObjectDTO dto){
-    daySchedule.add(dto);
+    trip.add(dto);
   }
 
   public void removeDaySchedule(ObjectDTO dto){
-    daySchedule.remove(dto);
+    trip.remove(dto);
   }
 
   public ObjectDTO getDaySchedule(int day){
-    return daySchedule.get(day);
+    return trip.get(day);
   }
 
 }
---
+++ app/src/main/java/com/anywhere/anywhere/insang/models/dto/api/TripDTO.java
@@ -0,0 +1,7 @@
+package com.anywhere.anywhere.insang.models.dto.api;
+
+import java.util.List;
+
+public class TripDTO extends ObjectDTO{
+    public List<DayScheduleDTO> daySchedule;
+}
